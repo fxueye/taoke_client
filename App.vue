10e478970f813735396f3687b51e7613135cf03a
skw <281431280@qq.com>
2020-05-04 22:24:58 +0800

    init app

--- App.vue
+++ App.vue
@@ -1,16 +1,7 @@
 <script>
 	export default {
-		globalData: {
-			text: 'text'
-		},
 		onLaunch: function() {
-			this.init().catch(() => {
-				console.log('init fail');
-			})
-			this.checkVersion().catch(() => {
-				console.log('checkversion fail');
-			});
-			// console.log('App Launch')
+			console.log('App Launch')
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -21,11 +12,6 @@
 	}
 </script>
 
-<style lang="scss">
+<style>
 	/*每个页面公共css */
-	/* #ifndef APP-PLUS */
-	@import "static/colorui/main.css";
-	@import "static/colorui/icon.css";
-	@import "static/app/style/app.scss";
-	/* #endif */
 </style>
--- common/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-	stage:"http://server.taoke.zhaobaoge.com",
-	server:"http://server.taoke.zhaobaoge.com",
-	channel:"android",
-	versionName:"1.0.0",
-	versionCode:'100'
-}
